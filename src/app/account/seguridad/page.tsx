8c9797a523d4833767b0a7ebd569dd750cf5d4fa
Juan David Arias <juadariasmar@gmail.com>
2026-06-24 12:49:57 -0500

    F4: /account/seguridad colapsable, Empresa en nav para todos, sesion actual

---
+++ src/app/account/seguridad/page.tsx
@@ -0,0 +1,10 @@
+import LayoutProtegido from '@/componentes/comunes/LayoutProtegido'
+import SeguridadColapsable from './SeguridadColapsable'
+
+export default function SeguridadPage() {
+  return (
+    <LayoutProtegido>
+      <SeguridadColapsable />
+    </LayoutProtegido>
+  )
+}
